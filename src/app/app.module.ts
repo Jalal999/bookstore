4b7f70cb4462d702790876d7311d367ffbff9d8b
jalal.mammadli <jmammadli2021@ada.edu.az>
2022-01-12 13:13:11 +0200

    product-list-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,27 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule, Routes } from '@angular/router';
-
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const appRoutes: Routes = [
-  {path: 'cart', component: ShoppingCartComponent}
+  {path: '', component: AppComponent},
+  {path: 'cart', component: ShoppingCartComponent},
+  {path: 'product', component: ProductDetailsComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductListComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +34,8 @@ const appRoutes: Routes = [
     MatToolbarModule,
     MatBadgeModule,
     MatButtonModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products.ts
@@ -0,0 +1,31 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    imgPath: string;
+  }
+  
+  export const products = [
+    {
+      id: 1,
+      name: 'Learn React',
+      price: 799,
+      description: 'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
+      imgPath: '../../../assets/images/book_image_1.jpg'
+    },
+    {
+      id: 2,
+      name: 'Java Programming Language',
+      price: 699,
+      description: 'Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.',
+      imgPath: '../../../assets/images/book_image_2.jpg'
+    },
+    {
+      id: 3,
+      name: 'Amazon Book',
+      price: 299,
+      description: 'Amazon Books is a customer-focused store, designed to spur discovery; a place where customers can find great books and products, and learn more about our services',
+      imgPath: '../../../assets/images/book_image_3.jpg'
+    }
+  ];
